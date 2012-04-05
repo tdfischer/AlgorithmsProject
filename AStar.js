4ee4610b4a68d7a6a57dfef36c16bf6907bcd524
Robert 'Nybbles' Studenic <rs.nibbles@gmail.com>
2012-04-04 23:00:53 -0400

    Generic initial AStar.js class.

---
+++ AStar.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * AStar.js : 
+ * A back-end class that will perform the A* path-finding algorithm, with multiple heuristics.
+ *
+ */
+
+/*
+ * Somewhere, this code will come into play to provide some prototypal inheritance :
+ * if (typeof Object.create !== 'function') {
+ *     Object.create = function (o) {
+ *     		function F() {}
+ *     		F.prototype = o;
+ *     		return new F();
+ *     };
+ * }
+ * newObject = Object.create(oldObject);
+ *
+ *
+ */
+
+
+// A*? AStar? A? Settle on a name.
+function AStar(start, end) {
+
+	// TODO : Code up the basic A* algorithm, with heuristics extracted out into another function.
+
+	// TODO : Fire events to the UI for every step through the algorithm.
+
+	// TODO : Return the path from start to end.
+}
