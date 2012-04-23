d9e10b17cf3a6db7a1dc875ca926aae64bef7537
Robert 'Nybbles' Studenic <rs.nibbles@gmail.com>
2012-04-23 18:59:58 -0400

    Fixed bounds on neighbors

--- src/AStar.java
+++ src/AStar.java
@@ -28,34 +28,34 @@ public class AStar {
             closedset.add(current);
             if (current == end) {
                 // Return the path to the start and end.
-                return null;
+                return reconstruct_path(end, start);
             }
 
             // Get the neighbors of the current node.
             ArrayList<MapObject> neighbors = new ArrayList<MapObject>();
             // Up
-            if (current.location.y - 1 < 0 && closedset.contains(map[current.location.x][current.location.y-1]))
+            if (current.location.y - 1 >= 0 && closedset.contains(map[current.location.x][current.location.y-1]))
                 neighbors.add(map[current.location.x][current.location.y-1]);
             // Down
-            if (current.location.y + 1 > MAP_WIDTH && closedset.contains(map[current.location.x][current.location.y+1))
+            if (current.location.y + 1 >= MAP_WIDTH && closedset.contains(map[current.location.x][current.location.y+1]))
                 neighbors.add(map[current.location.x][current.location.y+1]);
             // Left
-            if (current.location.x - 1 < 0 && closedset.contains(map[current.location.x-1][current.location.y]))
+            if (current.location.x - 1 >= 0 && closedset.contains(map[current.location.x-1][current.location.y]))
                 neighbors.add(map[current.location.x-1][current.location.y]);
             // Right
-            if (current.location.x + 1 > MAP_HEIGHT && closedset.contains(map[current.location.x+1][current.location.y]))
+            if (current.location.x + 1 >= MAP_HEIGHT && closedset.contains(map[current.location.x+1][current.location.y]))
                 neighbors.add(map[current.location.x+1][current.location.y]);
 
             // Check each neighbor.
             for (MapObject neighbor : neighbors) {
-                int cost = current.g_score + movement_cost(current, neighbor);
+                double cost = current.g_score + movement_cost(current, neighbor);
 
                 // Remove neighbor from openset, because new path is better.
                 if (openset.contains(neighbor) && cost <= neighbor.g_score)
                     openset.remove(neighbor);
                 // Remove neighbor from closedset, because we found a better path.
                 // This should never happen if the heuristic is monotonic.
-                if (closedset.contains(neighbor) && cost <= neighbor.g_score))
+                if (closedset.contains(neighbor) && cost <= neighbor.g_score)
                     closedset.remove(neighbor);
                 // If the neighbor hasn't been in either, add it to open.
                 if (!openset.contains(neighbor) && !closedset.contains(neighbor)) {
@@ -67,6 +67,16 @@ public class AStar {
         }
 
         return null;
+    }
+
+    private ArrayList<MapObject> reconstruct_path(MapObject current, MapObject start) {
+        ArrayList<MapObject> path = new ArrayList<MapObject>();
+
+        if (current.parent != start) {
+            path.addAll(reconstruct_path(current.parent, start));
+        }
+        path.add(current);
+        return path;
     }
 
     private int movement_cost(MapObject current, MapObject neighbor) {
