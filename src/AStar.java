32c3dad57c92206b3155a7ad362c0cdc0d907691
Robert 'Nybbles' Studenic <rs.nibbles@gmail.com>
2012-04-23 18:12:40 -0400

    Getting and setting the proper Map Objects into the openset and closedset.

--- src/AStar.java
+++ src/AStar.java
@@ -16,7 +16,20 @@ public class AStar {
     public ArrayList<MapObject> search(MapObject start, MapObject end) {
 
         PriorityQueue<MapObject> openset = new PriorityQueue<MapObject>();
+        // Add start to the openset.
+        openset.offer(start);
         HashSet<MapObject> closedset = new HashSet<MapObject>();
+
+        while (openset.size() > 0) {
+
+            MapObject current = openset.poll();
+            closedset.add(current);
+            if (current == end) {
+                // Return the path to the start and end.
+                return null;
+            }
+
+        }
 
         return null;
     }
